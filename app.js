1fe5b9792c878d833563ec3070722cd8d78c9562
Arnau Lacambra <naulacambra@gmail.com>
2017-10-07 16:48:26 +0200

    Make it work!

--- app.js
+++ app.js
@@ -4,13 +4,15 @@ A simple echo bot for the Microsoft Bot Framework.
 
 var restify = require('restify');
 var builder = require('botbuilder');
+var dotenv = require('dotenv');
+dotenv.config();
 
 // Setup Restify Server
 var server = restify.createServer();
 server.listen(process.env.port || process.env.PORT || 3978, function () {
    console.log('%s listening to %s', server.name, server.url); 
 });
-  
+
 // Create chat connector for communicating with the Bot Framework Service
 var connector = new builder.ChatConnector({
     appId: process.env.MicrosoftAppId,
