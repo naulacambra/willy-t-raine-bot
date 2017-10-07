325ef9556efb34a86f17c9aa66cbf108a575823c
Arnau Lacambra <naulacambra@gmail.com>
2017-10-07 16:49:28 +0200

    Test

--- app.js
+++ app.js
@@ -35,7 +35,7 @@ var bot = new builder.UniversalBot(connector);
 
 bot.dialog('/', [
     function (session) {
-        builder.Prompts.text(session, "Hello... What's your name?");
+        builder.Prompts.text(session, "Hello... What's your name? Motherfucker!");
     },
     function (session, results) {
         session.userData.name = results.response;
